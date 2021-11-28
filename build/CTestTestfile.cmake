745e18893c4a7c697c3f914cd7c04b3263969b88
Henry Chadban <HenryJ.Chadban@Gmail.com>
2021-11-28 21:40:07 +1100

    reconfigured project to enable automatic building using cmake

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/henry_chadban/Documents/C++ Projects/Euchre
-# Build directory: /Users/henry_chadban/Documents/C++ Projects/Euchre/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
