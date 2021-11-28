feaa9716010081f4c86d4fa3540c6b9b9ac0b3d3
Henry Chadban <HenryJ.Chadban@Gmail.com>
2021-11-28 21:23:57 +1100

    reconfigured build to work with cmake

---
+++ build/CMakeFiles/Euchre.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/henry_chadban/Documents/C++ Projects/Euchre/card.cpp" "CMakeFiles/Euchre.dir/card.cpp.o" "gcc" "CMakeFiles/Euchre.dir/card.cpp.o.d"
+  "/Users/henry_chadban/Documents/C++ Projects/Euchre/euchre_main.cpp" "CMakeFiles/Euchre.dir/euchre_main.cpp.o" "gcc" "CMakeFiles/Euchre.dir/euchre_main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/henry_chadban/Documents/C++ Projects/Euchre
+# Build directory: /Users/henry_chadban/Documents/C++ Projects/Euchre/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
